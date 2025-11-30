808777f7e4658b755591c6097e22422d714c5692
Prince Soni <nagvanshi781@gmail.com>
2025-11-30 19:12:07 +0530

    sql code change

--- ola.sql
+++ ola.sql
@@ -19,7 +19,10 @@ select COUNT(*) from bookings
 WHERE Booking_Status = 'Canceled by customer';
 select * from  Canceled_Rides_by_Customer;
 -- 4. List the top 5 customers who booked the highest number of rides:
-
+SELECT Customer_ID, COUNT(Booking_ID) as total_rides
+FROM bookings
+GROUP by Customer_ID
+OVER BY total_rides DESC LIMIT 5;
 -- 5. Get the number of rides canceled by drivers due to personal and car-related issues:
 
 -- 6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
