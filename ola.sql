49ef5bcbad57bfb9ab6a81292b0052d04bba63e1
Prince Soni <nagvanshi781@gmail.com>
2025-11-30 19:47:06 +0530

    changes

--- ola.sql
+++ ola.sql
@@ -26,13 +26,36 @@ GROUP by Customer_ID
 ORDER BY total_rides DESC LIMIT 5;
 select * from Top_5_Customers;
 -- 5. Get the number of rides canceled by drivers due to personal and car-related issues:
-
+ CREATE VIEW  Rides_cancelled_by_Drivers_P_C_Issues As
+ SELECT COUNT(*) FROM bookings 
+ WHERE cancelled_Rides_by_Driver = 'Personal & Car related issue';
+SELECT * FROM Rides_cancelled_by_Drivers_P_C_Issues;
 -- 6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
-
+Create View Max_Min_Driver_Rating As
+SELECT MAX(Driver_Ratings) as max_rating, MIN(Driver_Ratings) as min_rating 
+ FROM bookings 
+ WHERE Vehicle_Type = 'Prime Sedan';
+SELECT * FROM View Max_Min_Driver_Rating;
 -- 7. Retrieve all rides where payment was made using UPI:
-
--- 8. Find the average customer rating per vehicle type:
-
--- 9. Calculate the total booking value of rides completed successfully:
-
--- 10. List all incomplete rides along with the reason:
+Create View UPI_Payment As
+SELECT * FROM bookings 
+WHERE Payment_Method = 'UPI';
+SELECT * FROM  View UPI_Payment;
+ --8. Find the average customer rating per vehicle type:
+CREATE View AVG_Cust_Rating As
+ SELECT Vehicle_Type, AVG(Customer_Rating) as avg_customer_rating 
+ FROM bookings
+ GROUP BY Vehicle_Type;
+SELECT * FROM AVG_Cust_Rating;
+ --9. Calculate the total booking value of rides completed successfully:
+Create View total_successful_ride_value As
+ SELECT SUM(Booking_Value) as total_successful_value
+ FROM bookings 
+ WHERE Booking_Status = 'Success';
+SELECT * FROM total_successful_ride_value;
+ --10. List all incomplete rides along with the reason:
+Create View Incomplete_Rides_Reason As
+ SELECT Booking_ID, Incomplete_Rides_Reason 
+ FROM bookings 
+ WHERE Incomplete_Rides ='Yes'
+SELECT * FROM Incomplete_Rides_Reason;
